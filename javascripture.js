f8ad0c9ce33a9b74bb2d52541708dbaea07a2944
Free8Spirit <38473227+Free8Spirit@users.noreply.github.com>
2018-09-30 17:28:45 -0500

    deleted prelayout commits on js source file

--- javascripture.js
+++ javascripture.js
@@ -1,24 +1,5 @@
 // JavaScript source code
 
-// two containers; one for the first screen that will be displayed; second for when they hit the button at bottom of the page below the form;
-
-// form tag holding inside: input tag with type = text; 2; seperate id names for the user to input name and second one for phone number;
-
-// selection tag with the numbers 1 - 10;
-
-//js goes here. (ill do the complex part)
-
-// button tag with the text "Order" or "Place Order";
-
-// simple math with refrencing sources (I can do this after words);
-
-// in the second container, a div for the name and number then another div for the order (id for each to call below);
-
-// another button with text "Place Another Order?" or something else.
-
-//var name = document.getElementById('name');
-//var numPhone = document.getElementById('number');
-
 var but1 = document.getElementById('Order');
 var but2 = document.getElementById('home');
 
